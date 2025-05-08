d3919e3e432e4f916ee22fde29b560bcd28dfa3b
igor <inavitski@tenna.com>
2025-05-08 16:28:23 +0300

    add adaptive styles

--- src/components/sidebar/Sidebar.tsx
+++ src/components/sidebar/Sidebar.tsx
@@ -1,4 +1,4 @@
-import { FC } from 'react';
+import { FC, useState } from 'react';
 import { NavLink } from 'react-router';
 
 import { MAIN_ROUTES } from '../../constants/routes';
@@ -6,11 +6,16 @@ import { MAIN_ROUTES } from '../../constants/routes';
 import classes from './sidebar.module.scss';
 
 const Sidebar: FC = () => {
+  const [isExpanded, setIsExpanded] = useState(false);
+
   return (
     <div className={classes.container}>
+      <label htmlFor="toggle" className={classes.burger} onClick={() => setIsExpanded(!isExpanded)} />
+      <input type="checkbox" checked={isExpanded} id="toggle" name="toggle" className={classes.toggle}></input>
+
       <div className={classes.navigation}>
         {MAIN_ROUTES.map(route => (
-          <NavLink key={route.name} to={route.path} className={classes.link}>
+          <NavLink key={route.name} to={route.path} className={classes.link} onClick={() => setIsExpanded(false)}>
             {route.name}
           </NavLink>
         ))}
