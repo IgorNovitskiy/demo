f512b8aa7579d748a7981f6b5114945555d41de6
igor <inavitski@tenna.com>
2025-05-09 11:22:43 +0300

    init Sentry

--- src/main.tsx
+++ src/main.tsx
@@ -1,3 +1,4 @@
+import * as Sentry from '@sentry/react';
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 
@@ -5,6 +6,11 @@ import './styles/colors.css';
 import './styles/reset.css';
 
 import App from './App';
+
+Sentry.init({
+  dsn: import.meta.env.VITE_SENTRY_DSN,
+  sendDefaultPii: true,
+});
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
